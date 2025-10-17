68babadbe43e3907c23045775cbe715b6e9b014e
hailee2 <slowslow9150@gmail.com>
2025-10-17 22:03:35 +0900

    refactor/product: UserEntity->User로 변경 (rename UserEntity to User)

--- src/main/java/org/example/lastcall/domain/product/sevice/ProductService.java
+++ src/main/java/org/example/lastcall/domain/product/sevice/ProductService.java
@@ -7,16 +7,19 @@ import org.example.lastcall.domain.product.dto.response.ProductResponse;
 import org.example.lastcall.domain.product.entity.Product;
 import org.example.lastcall.domain.product.exception.ProductErrorCode;
 import org.example.lastcall.domain.product.repository.ProductRepository;
+import org.example.lastcall.domain.user.entity.User;
+import org.example.lastcall.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService implements ProductServiceApi {
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 
     public ProductResponse createProduct(Long userId, ProductCreateRequest request) {
-        User user = UserRepository.findById(userId)
-                .orElseThorw(() -> new BusinessException(ProductErrorCode.USER_NOT_FOUND));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ProductErrorCode.USER_NOT_FOUND));
         Product product = Product.of(user, request.getName(), request.getCategory(), request.getDescription());
         Product savedProduct = productRepository.save(product);
 

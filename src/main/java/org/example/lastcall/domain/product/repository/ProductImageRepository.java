e0c5430a339b743ddde43117d7c4ea32e8cff5f9
hailee2 <slowslow9150@gmail.com>
2025-10-19 00:43:48 +0900

    fix/product: 대표 이미지 조회 API에 imageType을 request param으로 변경(change product image API to accept imageType as request parameter)

--- src/main/java/org/example/lastcall/domain/product/repository/ProductImageRepository.java
+++ src/main/java/org/example/lastcall/domain/product/repository/ProductImageRepository.java
@@ -1,10 +1,15 @@
 package org.example.lastcall.domain.product.repository;
 
+import org.example.lastcall.domain.product.entity.ImageType;
 import org.example.lastcall.domain.product.entity.ProductImage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
     List<ProductImage> findAllByProductId(Long productId);
+
+    Page<ProductImage> findAllByImageType(ImageType imageType, Pageable pageable);
 }

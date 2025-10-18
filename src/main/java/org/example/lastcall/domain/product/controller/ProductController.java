602728abd2cabf08b59aaceb6140a3361bd40b36
hailee2 <slowslow9150@gmail.com>
2025-10-18 20:38:36 +0900

    feat/product: 상품 전체 조회, 상품 단건 조회, 상품별 이미지 전체 조회, 대표 이미지 전체 조회 컨트롤러 구현(Implement controllers for: full product listing, single product, all images per product, and all thumbnail (representative) images.)

--- src/main/java/org/example/lastcall/domain/product/controller/ProductController.java
+++ src/main/java/org/example/lastcall/domain/product/controller/ProductController.java
@@ -3,15 +3,15 @@ package org.example.lastcall.domain.product.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.lastcall.common.response.ApiResponse;
+import org.example.lastcall.common.response.PageResponse;
 import org.example.lastcall.domain.product.dto.request.ProductCreateRequest;
+import org.example.lastcall.domain.product.dto.response.ProductReadAllResponse;
 import org.example.lastcall.domain.product.dto.response.ProductResponse;
 import org.example.lastcall.domain.product.sevice.ProductServiceApi;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +26,22 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 ApiResponse.success("상품을 등록했습니다.", productService.createProduct(userId, request))
         );
+    }
+
+    //상품 전체 조회(상품 아이디와 상품명만 조회)
+    @GetMapping
+    public ResponseEntity<ApiResponse<PageResponse<ProductReadAllResponse>>> readAllProduct(Pageable pageable) {
+        PageResponse<ProductReadAllResponse> pageResponse = productService.readAllProduct(pageable.getPageNumber(), pageable.getPageSize());
+        ApiResponse<PageResponse<ProductReadAllResponse>> apiResponse = ApiResponse.success("상품을 전체 조회했습니다.", pageResponse);
+
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    //상품 단건 조회
+    @GetMapping("/{productId}")
+    public ResponseEntity<ApiResponse<ProductResponse>> readProduct(@PathVariable Long productId) {
+        ProductResponse response = productService.readProduct(productId);
+        ApiResponse<ProductResponse> apiResponse = ApiResponse.success("상품 단건 조회에 성공했습니다.", response);
+        return ResponseEntity.ok(apiResponse);
     }
 }
--- src/main/java/org/example/lastcall/domain/product/controller/ProductImageController.java
+++ src/main/java/org/example/lastcall/domain/product/controller/ProductImageController.java
@@ -2,12 +2,17 @@ package org.example.lastcall.domain.product.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.lastcall.common.response.ApiResponse;
+import org.example.lastcall.common.response.PageResponse;
 import org.example.lastcall.domain.product.dto.request.ProductImageCreateRequest;
+import org.example.lastcall.domain.product.dto.response.ProductImageReadAllResponse;
 import org.example.lastcall.domain.product.dto.response.ProductImageResponse;
 import org.example.lastcall.domain.product.sevice.ProductImageServiceApi;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +26,21 @@ public class ProductImageController {
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 ApiResponse.success("상품 이미지를 등록했습니다.", productImageService.createProductImage(productId, request))
         );
+    }
+
+    //이미지 전체 조회(상품아이디와 썸네일만 "/api/v1/products/image?imageType=Thumbnail")
+    @GetMapping("/image?imageType=Thumbnail")
+    public ResponseEntity<ApiResponse<PageResponse<ProductImageReadAllResponse>>> readThumbnailImages(Pageable pageable) {
+        PageResponse<ProductImageReadAllResponse> pageResponse = productImageService.readAllThumbnailImage(pageable.getPageNumber(), pageable.getPageSize());
+        ApiResponse<PageResponse<ProductImageReadAllResponse>> apiResponse = ApiResponse.success("대표 이미지 전체 조회 성공했습니다.", pageResponse);
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    //상품별 이미지 전체 조회
+    @GetMapping("/{productId}/image")
+    public ResponseEntity<ApiResponse<List<ProductImageResponse>>> readAllProductImage(@PathVariable Long productId) {
+        List<ProductImageResponse> response = productImageService.readAllProductImage(productId);
+        ApiResponse<List<ProductImageResponse>> apiResponse = ApiResponse.success("", response);
+        return ResponseEntity.ok(apiResponse);
     }
 }

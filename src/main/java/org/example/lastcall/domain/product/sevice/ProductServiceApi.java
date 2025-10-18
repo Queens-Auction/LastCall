507060a951c7f6e5606f6b97400b3ad541c8ada1
hailee2 <slowslow9150@gmail.com>
2025-10-18 19:19:40 +0900

    refact/product: 상품 전체 조회, 대표 이미지 전체 조회 공통 페이징 처리(Common pagination for full product list and main (thumbnail) image list)

--- src/main/java/org/example/lastcall/domain/product/sevice/ProductServiceApi.java
+++ src/main/java/org/example/lastcall/domain/product/sevice/ProductServiceApi.java
@@ -1,16 +1,15 @@
 package org.example.lastcall.domain.product.sevice;
 
+import org.example.lastcall.common.response.PageResponse;
 import org.example.lastcall.domain.product.dto.request.ProductCreateRequest;
 import org.example.lastcall.domain.product.dto.response.ProductReadAllResponse;
 import org.example.lastcall.domain.product.dto.response.ProductResponse;
-
-import java.util.List;
 
 public interface ProductServiceApi {
     ProductResponse createProduct(Long userId, ProductCreateRequest request);
 
     //상품 전체 조회
-    List<ProductReadAllResponse> readAllProduct();
+    PageResponse<ProductReadAllResponse> readAllProduct(int page, int size);
 
     //상품 단건 조회
     ProductResponse readProduct(Long productId);

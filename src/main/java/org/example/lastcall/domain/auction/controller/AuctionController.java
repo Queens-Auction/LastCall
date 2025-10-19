98bef9772474ad047ac04648f4f7e8bdb6a0dd83
jinhee kim <lsa68803@naver.com>
2025-10-19 14:47:27 +0900

    fix/auction: 컨트롤러 경로 오타 수정 (fix typo in request mapping '/api/v1/auctions')

--- src/main/java/org/example/lastcall/domain/auction/controller/AuctionController.java
+++ src/main/java/org/example/lastcall/domain/auction/controller/AuctionController.java
@@ -4,26 +4,23 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.lastcall.common.response.ApiResponse;
 import org.example.lastcall.domain.auction.dto.request.AuctionCreateRequest;
-import org.example.lastcall.domain.auction.dto.response.AuctionResponse;
+import org.example.lastcall.domain.auction.dto.response.AuctionCreateResponse;
 import org.example.lastcall.domain.auction.service.AuctionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/actions")
+@RequestMapping("/api/v1/auctions") // 오타 수정
 public class AuctionController {
     private final AuctionService auctionService;
 
     // 경매 등록 //
     @PostMapping
-    public ResponseEntity<ApiResponse<AuctionResponse>> createAuction(Long userId,
-                                                                      // 시큐리티 적용 후, @AuthenticationPrincipal AuthUser authUser 로 변경 예정
-                                                                      @Valid @RequestBody AuctionCreateRequest request) {
+    public ResponseEntity<ApiResponse<AuctionCreateResponse>> createAuction(@RequestHeader("userId") Long userId,
+                                                                            // 시큐리티 적용 후, @AuthenticationPrincipal AuthUser authUser 로 변경 예정
+                                                                            @Valid @RequestBody AuctionCreateRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 ApiResponse.success("경매가 등록되었습니다.", auctionService.createAuction(userId, request))
         );

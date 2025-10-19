2e137786157fd66db6c529aea00ae72ef2ab5502
jinhee kim <lsa68803@naver.com>
2025-10-19 14:31:03 +0900

    feat/auction: AuctionCreateResponse에 빌더 추가 (add @Builder)

--- src/main/java/org/example/lastcall/domain/auction/dto/response/AuctionCreateResponse.java
+++ src/main/java/org/example/lastcall/domain/auction/dto/response/AuctionCreateResponse.java
@@ -1,11 +1,13 @@
 package org.example.lastcall.domain.auction.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import org.example.lastcall.domain.auction.entity.AuctionStatus;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
 public class AuctionCreateResponse {
     private Long auctionId;
     private Long productId;

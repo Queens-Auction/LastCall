969e4f01f10979eb74fbbf7a78d41b52c71491ce
hailee2 <slowslow9150@gmail.com>
2025-10-20 21:48:08 +0900

    refact/product: serviceApi 적용하도록 수정 (adjust serviceApi)

--- src/main/java/org/example/lastcall/domain/product/sevice/ProductServiceApi.java
+++ src/main/java/org/example/lastcall/domain/product/sevice/ProductServiceApi.java
@@ -1,16 +1,11 @@
 package org.example.lastcall.domain.product.sevice;
 
-import org.example.lastcall.common.response.PageResponse;
-import org.example.lastcall.domain.product.dto.request.ProductCreateRequest;
-import org.example.lastcall.domain.product.dto.response.ProductReadAllResponse;
 import org.example.lastcall.domain.product.dto.response.ProductResponse;
+import org.example.lastcall.domain.product.entity.Product;
 
 public interface ProductServiceApi {
-    ProductResponse createProduct(Long userId, ProductCreateRequest request);
-
-    //상품 전체 조회
-    PageResponse<ProductReadAllResponse> readAllProduct(int page, int size);
-
     //상품 단건 조회
     ProductResponse readProduct(Long productId);
+
+    Product findById(Long productId);
 }

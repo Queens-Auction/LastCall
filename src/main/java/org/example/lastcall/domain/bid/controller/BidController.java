977556138f4fc0c00d3d993414352d74b616d4d2
eunji-0508 <cojia@naver.com>
2025-10-21 12:08:05 +0900

    feat/bid: 입찰 등록 기능 구현 (implement bid creation feature)

--- src/main/java/org/example/lastcall/domain/bid/controller/BidController.java
+++ src/main/java/org/example/lastcall/domain/bid/controller/BidController.java
@@ -1,10 +1,28 @@
 package org.example.lastcall.domain.bid.controller;
 
+import org.example.lastcall.common.response.ApiResponse;
+import org.example.lastcall.domain.bid.dto.response.BidResponse;
+import org.example.lastcall.domain.bid.service.BidService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/auctions/{auctionId}/bids")
 public class BidController {
+	private final BidService bidService;
+
+	// 입찰 등록
+	@PostMapping
+	public ResponseEntity<ApiResponse<BidResponse>> createBid(@PathVariable Long auctionId, Long userId) {
+		BidResponse bid = bidService.createBid(auctionId, userId);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success("입찰이 완료되었습니다.", bid));
+	}
 }

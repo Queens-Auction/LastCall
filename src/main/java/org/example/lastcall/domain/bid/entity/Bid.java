55312906cb7c3c95bb66ad86f07be7a0c30d2e7b
eunjin0468 <eunjin0468@naver.com>
2025-10-21 09:17:57 +0900

    fix/build-bid: 빌드 설정 및 Bid 엔티티 오류 수정 (Fix build configuration and Bid entity issue)

--- src/main/java/org/example/lastcall/domain/bid/entity/Bid.java
+++ src/main/java/org/example/lastcall/domain/bid/entity/Bid.java
@@ -14,12 +14,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.example.lastcall.domain.auction.entity.Auction;
-import org.example.lastcall.domain.user.entity.User;
 
 @Entity
 @Getter

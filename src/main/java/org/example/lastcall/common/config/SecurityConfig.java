5fcab3f07f0537d9b986b5bc897bab953772218a
eunjin0468 <eunjin0468@naver.com>
2025-10-21 12:31:32 +0900

    feat/auth: JWT 인증 필터 구현 (implement JwtAuthenticationFilter)

--- src/main/java/org/example/lastcall/common/config/SecurityConfig.java
+++ src/main/java/org/example/lastcall/common/config/SecurityConfig.java
@@ -1,10 +1,35 @@
 package org.example.lastcall.common.config;
 
+import lombok.RequiredArgsConstructor;
+import org.example.lastcall.domain.auth.jwt.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 //@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-//    private final JwtAuthenticationFilter jwtFilter;
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/email-verifications").permitAll()
+                        .requestMatchers(HttpMethod.PUT,  "/api/v1/email-verifications/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 }

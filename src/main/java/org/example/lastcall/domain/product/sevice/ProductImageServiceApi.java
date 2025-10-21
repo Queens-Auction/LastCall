a4d48e85091e731b9b48e48ac0ca195c7f32c228
hailee2 <slowslow9150@gmail.com>
2025-10-21 15:24:29 +0900

    feat/productImage: 상품별 전체 이미지 조회, 대표 이미지(썸네일)조회 메서드 구현(Implement methods for retrieving all images by product and retrieving the thumbnail (representative) image.)

--- src/main/java/org/example/lastcall/domain/product/sevice/ProductImageServiceApi.java
+++ src/main/java/org/example/lastcall/domain/product/sevice/ProductImageServiceApi.java
@@ -1,4 +1,13 @@
 package org.example.lastcall.domain.product.sevice;
 
+import org.example.lastcall.domain.product.dto.response.ProductImageResponse;
+
+import java.util.List;
+
 public interface ProductImageServiceApi {
+    // 대표 이미지(썸네일) 조회
+    ProductImageResponse readThumbnailImage(Long productId);
+
+    // 상품별 이미지리스트 조회
+    List<ProductImageResponse> readAllProductImage(Long productId);
 }

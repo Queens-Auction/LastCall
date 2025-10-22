0019c95d853f38bf9e88ee2e9f5b2c7eede7afa2
grace-jsy <sseo.oee@gmail.com>
2025-10-23 08:51:04 +0900

    refactor/point: PointLog에 Auction 엔티티 연관관계 추가 및 create() 메서드 파라미터 타입 수정(Add Auction entity relationship to PointLog and update create() method parameter type)

--- src/main/java/org/example/lastcall/domain/point/service/PointService.java
+++ src/main/java/org/example/lastcall/domain/point/service/PointService.java
@@ -162,7 +162,7 @@ public class PointService implements PointServiceApi {
 
     // 경매 종료 후 입찰 확정시 예치 포인트를 정산포인트로 이동
     @Override
-    public void DepositToSettlement(Long userId, Long amount) {
+    public void DepositToSettlement(Long userId, Long auctionId, Long amount) {
 
         // 낙찰자의 포인트 계좌 조회
         Point point = pointRepository.findByUserId(userId).orElseThrow(
@@ -181,7 +181,8 @@ public class PointService implements PointServiceApi {
                 point.getUser(),
                 PointLogType.SETTLEMENT,
                 "입찰 확정으로 인한 정산 포인트 이동",
-                amount
+                amount,
+                auctionRepository.getReferenceById(auctionId)
         );
 
         // 포인트 로그에 저장

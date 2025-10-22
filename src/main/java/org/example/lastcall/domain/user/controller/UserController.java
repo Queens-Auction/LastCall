7403138f84243edf2572146f4dc4c235868ad101
eunjin0468 <eunjin0468@naver.com>
2025-10-22 23:07:49 +0900

    feat/user: 회원 정보 수정 API 구현(implement user profile update API)

--- src/main/java/org/example/lastcall/domain/user/controller/UserController.java
+++ src/main/java/org/example/lastcall/domain/user/controller/UserController.java
@@ -4,11 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.example.lastcall.common.response.ApiResponse;
 import org.example.lastcall.common.security.Auth;
 import org.example.lastcall.domain.auth.model.AuthUser;
+import org.example.lastcall.domain.user.dto.request.UserUpdateRequest;
 import org.example.lastcall.domain.user.dto.response.UserProfileResponse;
 import org.example.lastcall.domain.user.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +19,13 @@ public class UserController {
     public ApiResponse<UserProfileResponse> getProfile(@Auth AuthUser authUser) {
         UserProfileResponse dto = userService.getUserById(authUser.userId());
         return ApiResponse.success("내 정보 조회 성공", dto);
+    }
+
+    @PatchMapping("/me")
+    public ApiResponse<UserProfileResponse> updateMe(@Auth AuthUser authUser,
+                                                     @RequestBody UserUpdateRequest request)
+    {
+        UserProfileResponse response = userService.updateMyProfile(authUser.userId(), request);
+        return ApiResponse.success("내 정보 수정 성공", response);
     }
 }

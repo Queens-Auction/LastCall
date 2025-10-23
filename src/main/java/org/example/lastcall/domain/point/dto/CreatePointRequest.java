108d1f8054b44f117e5afb0a84e6490035e3402c
jinhee kim <lsa68803@naver.com>
2025-10-23 23:39:43 +0900

    fix/point: 입찰 예치금이 포인트 조회에 반영되지 않는 문제 수정 (fix deposit reflection issue on point query)

--- src/main/java/org/example/lastcall/domain/point/dto/CreatePointRequest.java
+++ src/main/java/org/example/lastcall/domain/point/dto/CreatePointRequest.java
@@ -7,6 +7,7 @@ import org.example.lastcall.domain.point.entity.PointLogType;
 public class CreatePointRequest {
 
     private Long userId;              // 사용자 ID
+    private Long bidId;
     private PointLogType type;        // 포인트 변동 유형 (EARN, USE, ect...)
     private String description;       // 변동 사유
     private Long incomePoint;         // 입금된 포인트

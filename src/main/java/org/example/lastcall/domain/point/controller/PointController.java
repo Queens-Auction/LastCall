66ff842a2f2bfb07e2c951c12abb121c1cb44b19
grace-jsy <sseo.oee@gmail.com>
2025-10-27 14:32:11 +0900

    fix/point : 충돌 해결 (resolve conflicts)

--- src/main/java/org/example/lastcall/domain/point/controller/PointController.java
+++ src/main/java/org/example/lastcall/domain/point/controller/PointController.java
@@ -8,7 +8,6 @@ import org.example.lastcall.common.security.Auth;
 import org.example.lastcall.domain.auth.enums.AuthUser;
 import org.example.lastcall.domain.point.dto.request.CreatePointRequest;
 import org.example.lastcall.domain.point.dto.response.PointResponse;
-import org.example.lastcall.domain.auth.enums.AuthUser;
 import org.example.lastcall.domain.point.service.PointService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "포인트(Point) API", description = "사용자 포인트 충전 및 조회 기능 제공")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users/points")
 public class PointController {
 
     private final PointService pointService;
@@ -28,7 +27,7 @@ public class PointController {
             description = "로그인한 사용자가 지정된 금액만큼 포인트를 충전합니다. " +
                     "요청 시 본인 인증이 필요하며, 충전 금액은 0보다 커야 합니다."
     )
-    @PostMapping("/users/{userId}/points/earn")
+    @PostMapping("/earn")
     public ResponseEntity<PointResponse> createPoint(
             @Auth AuthUser authUser,
             @RequestBody @Valid CreatePointRequest request) {
@@ -43,7 +42,7 @@ public class PointController {
             summary = "포인트 조회",
             description = "로그인한 사용자의 현재 포인트 잔액을 조회합니다."
     )
-    @GetMapping("/users/{userId}/points")
+    @GetMapping
     public ResponseEntity<PointResponse> getUserPoint(@Auth AuthUser authUser) {
 
         PointResponse pointResponse = pointService.getUserPoint(authUser);

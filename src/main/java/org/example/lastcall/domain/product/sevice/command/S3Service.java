3db660e3c7a7ba0a3fb205ac9855182c9712ca45
hailee2 <slowslow9150@gmail.com>
2025-11-03 20:38:05 +0900

    feat/product: 상품 이미지 S3 업로드를 위한 S3Service 추가(Add S3Service for uploading product images to AWS S3)

---
+++ src/main/java/org/example/lastcall/domain/product/sevice/command/S3Service.java
@@ -0,0 +1,34 @@
+package org.example.lastcall.domain.product.sevice.command;
+
+import lombok.RequiredArgsConstructor;
+import org.example.lastcall.common.exception.BusinessException;
+import org.example.lastcall.domain.product.exception.ProductErrorCode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+    private final S3Client s3Client;
+    private final String bucketName = "queens-auction-bucket";
+
+    public String uploadToS3(MultipartFile file, String directory) {
+        try {
+            String fileName = directory + "/" + System.currentTimeMillis() + "_" + file.getOriginalFilename();
+
+            s3Client.putObject(builder -> builder
+                            .bucket(bucketName)
+                            .key(fileName)
+                            .contentType(file.getContentType())
+                            .build(),
+                    software.amazon.awssdk.core.sync.RequestBody.fromBytes(file.getBytes()));
+
+            return "https://" + bucketName + ".s3.ap-northeast-2.amazonaws.com/" + fileName;
+        } catch (IOException e) {
+            throw new BusinessException(ProductErrorCode.RUNTIME_EXCEPTION);
+        }
+    }
+}

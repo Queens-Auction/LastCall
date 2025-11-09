60fd8da6c07886aba9ca947ea2df0c86d72914e5
jinhee kim <lsa68803@naver.com>
2025-11-09 15:08:27 +0900

    feat/auction: 경매 시작/종료 이벤트 큐 및 라우팅 구조 분리 (split start/end routing and queues for auction events)

--- src/main/java/org/example/lastcall/common/config/AuctionConfig.java
+++ src/main/java/org/example/lastcall/common/config/AuctionConfig.java
@@ -14,10 +14,16 @@ import java.util.Map;
 @Configuration
 public class AuctionConfig {
     // 교환기(Exchange), 큐(Queue), 라우팅 키 를 상수 정의
+    // 교환기
     public static final String EXCHANGE_NAME = "auction.exchange";
-    public static final String QUEUE_NAME = "auction.queue";
-    public static final String ROUTING_KEY = "auction.key";
+    // 큐 이름
+    public static final String START_QUEUE_NAME = "auction.start.queue";
+    public static final String END_QUEUE_NAME = "auction.end.queue";
+    // 라우팅 키
+    public static final String START_ROUTING_KEY = "auction.start.key";
+    public static final String END_ROUTING_KEY = "auction.end.key";
 
+    // [공용] //
     // Delay Exchange 설정 (지연 큐)
     // -> 메시지 바로 큐로 보내지 않고, 지정된 시간(x-delay)만큼 대기 후 전달
     @Bean
@@ -25,21 +31,6 @@ public class AuctionConfig {
         Map<String, Object> args = new HashMap<>();
         args.put("x-delayed-type", "direct");
         return new CustomExchange(EXCHANGE_NAME, "x-delayed-message", true, false, args);
-    }
-
-    // 메시지 저장할 Queue 생성 (지속성 true)
-    @Bean
-    public Queue queue() {
-        return new Queue(QUEUE_NAME);
-    }
-
-    // Exchange 와 Queue 를 ROUTING_KEY 로 연결
-    @Bean
-    public Binding binding(Queue queue, CustomExchange delayExchange) {
-        // to.(delayExchange) : 메시지가 통과할 교환기 지정 -> 이 메시지는 지연큐 통해 들어옴을 명시
-        // .with(ROUTING_KEY) : 어떤 라우팅 키로 메시지 구분할지 결정
-        // .noargs() : .with()로 지정한 라우팅 키 외에 추가가 없어 기본 연결로 끝낸다는 뜻
-        return BindingBuilder.bind(queue).to(delayExchange).with(ROUTING_KEY).noargs();
     }
 
     // Jackson 사용한 JSON 직렬화/역직렬화 변환기(Converter)
@@ -56,5 +47,21 @@ public class AuctionConfig {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter());
         return rabbitTemplate;
+    }
+
+    // [경매 종료 전용]
+    // 메시지 저장할 Queue 생성 (지속성 true)
+    @Bean
+    public Queue queue() {
+        return new Queue(END_QUEUE_NAME);
+    }
+
+    // Exchange 와 Queue 를 ROUTING_KEY 로 연결
+    @Bean
+    public Binding binding(Queue queue, CustomExchange delayExchange) {
+        // to.(delayExchange) : 메시지가 통과할 교환기 지정 -> 이 메시지는 지연큐 통해 들어옴을 명시
+        // .with(ROUTING_KEY) : 어떤 라우팅 키로 메시지 구분할지 결정
+        // .noargs() : .with()로 지정한 라우팅 키 외에 추가가 없어 기본 연결로 끝낸다는 뜻
+        return BindingBuilder.bind(queue).to(delayExchange).with(END_ROUTING_KEY).noargs();
     }
 }

481c3e3280ff63c84d7e0a062571b00ac854c96e
jinhee kim <lsa68803@naver.com>
2025-11-09 12:10:56 +0900

    feat/auction: 경매 종료 시 포인트 반환 로직 연동 (integrate point refund on auction close)

--- src/main/java/org/example/lastcall/domain/auction/service/command/AuctionCommandService.java
+++ src/main/java/org/example/lastcall/domain/auction/service/command/AuctionCommandService.java
@@ -9,16 +9,20 @@ import org.example.lastcall.domain.auction.entity.Auction;
 import org.example.lastcall.domain.auction.enums.AuctionStatus;
 import org.example.lastcall.domain.auction.exception.AuctionErrorCode;
 import org.example.lastcall.domain.auction.repository.AuctionRepository;
+import org.example.lastcall.domain.auction.service.event.AuctionEvent;
+import org.example.lastcall.domain.auction.service.event.AuctionEventPublisher;
 import org.example.lastcall.domain.bid.entity.Bid;
 import org.example.lastcall.domain.bid.service.query.BidQueryServiceApi;
 import org.example.lastcall.domain.point.service.command.PointCommandService;
-import org.example.lastcall.domain.point.service.query.PointQueryServiceApi;
 import org.example.lastcall.domain.product.entity.Product;
 import org.example.lastcall.domain.product.service.query.ProductQueryServiceApi;
 import org.example.lastcall.domain.user.entity.User;
 import org.example.lastcall.domain.user.service.UserServiceApi;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
@@ -28,9 +32,10 @@ public class AuctionCommandService implements AuctionCommandServiceApi {
     private final AuctionRepository auctionRepository;
     private final UserServiceApi userServiceApi;
     private final ProductQueryServiceApi productQueryService;
-    private final PointQueryServiceApi pointQueryServiceApi;
     private final BidQueryServiceApi bidQueryServiceApi;
     private final PointCommandService pointCommandServiceApi;
+    private final RabbitTemplate rabbitTemplate;
+    private final AuctionEventPublisher auctionEventPublisher;
 
     // 경매 등록 //
     public AuctionResponse createAuction(Long productId, Long userId, AuctionCreateRequest request) {
@@ -44,9 +49,39 @@ public class AuctionCommandService implements AuctionCommandServiceApi {
         User user = userServiceApi.findById(userId);
         // 4. 상품 조회
         Product product = productQueryService.findById(productId);
-        // 5. 경매 등록
+        // 5. Auction 엔티티 생성/저장 -> 경매 상태는 내부 로직에서 자동 계산됨
         Auction auction = Auction.of(user, product, request);
         auctionRepository.save(auction);
+        // [RabbitMq 관련]
+
+        /** 6. 경매 종료까지 남은 시간 계산
+         * - Duration.between(현재시간, 종료시간)
+         * - toMillis() : 밀리초 단위로 변환하여 delay 설정에 사용
+         */
+        Long delay = Duration.between(
+                auction.getCreatedAt(),   // createAt 써야함. (예약된 경매도 있으므로)
+                auction.getEndTime()
+        ).toMillis();
+
+        /** 7. 메시지큐로 경매 종료 이벤트 메시지 생성
+         * - 이 이벤트 객체는 메시지큐에 전송되어 종료시점이 되면,
+         * - Consumer (AuctionEventListener)가 꺼내서 closeAuction() 호출
+         * - 낙찰자/낙찰가/유찰자는 종료 시점에 계산되므로 지금은 null
+         */
+        AuctionEvent event = new AuctionEvent(
+                auction.getId(),
+                null,      // 낙찰자 x -> 종료 시 계산
+                null,              // 낙찰가 x -> 종료 시 계산
+                null               // 유찰자 목록 x -> 종료 시 계산
+        );
+
+        /**8. 메시지큐에 경매 종료 이벤트 발행
+         * - AuctionEventPublisher 내부에서 RabbitTemplate.convertAndSend() 사용
+         * - x-delayed-message Exchange 를 통해 delay 만큼 대기 후 큐로 전달
+         * - RabbitMQ 가 지연시간(delayMillis) 이후 케시지를 큐로 push
+         * - AuctionEventListener 가 이를 수신하여 closeAuction() 자동 실행
+         */
+        auctionEventPublisher.sendAuctionEndEvent(event, delay);
 
         return AuctionResponse.fromCreate(auction);
     }
@@ -95,14 +130,20 @@ public class AuctionCommandService implements AuctionCommandServiceApi {
         Bid topBid = bidQueryServiceApi.findTopByAuctionOrderByBidAmountDesc(auction).orElse(null);
 
         if (topBid != null) {
+            // 입찰 존재 시, 낙찰 처리
             Long winnerId = topBid.getUser().getId();
             Long bidAmount = topBid.getBidAmount();
 
-            // 낙찰자 호출
+            // 낙찰 처리
             auction.assignWinner(winnerId, bidAmount);
 
             // 포인트 처리 (예치 -> 가용)
             pointCommandServiceApi.depositToAvailablePoint(winnerId, auction.getId(), bidAmount);
+
+            // 잘되는지 테스트용
+            System.out.printf("경매 종료 - 낙찰자 id: %d, 낙찰가: %d원%n", winnerId, bidAmount);
+        } else {
+            auction.closeAsFailed();
         }
         auctionRepository.save(auction);
     }
--- src/main/java/org/example/lastcall/domain/auction/service/event/AuctionEventListener.java
+++ src/main/java/org/example/lastcall/domain/auction/service/event/AuctionEventListener.java
@@ -1,16 +1,34 @@
 package org.example.lastcall.domain.auction.service.event;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.lastcall.common.config.AuctionConfig;
+import org.example.lastcall.domain.auction.service.command.AuctionCommandService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service  // 메시지 수신 역할 하는 서비스
+@RequiredArgsConstructor
+@Slf4j
 public class AuctionEventListener {
+    private final AuctionCommandService auctionCommandService;
+
     // 큐에 쌓인 경매 종료 이벤트를 자동으로 수신하여 처리
     @RabbitListener(queues = AuctionConfig.QUEUE_NAME)
-    public void handleAuctionEnd(AuctionEvent event) {
-        // 메시지 들어오면 실행되는 부분
+    /*public void handleAuctionEnd(AuctionEvent event) {
+        // 메시지 들어오면 실행되는 부분 -> 테스트 후 삭제하기
         System.out.println("경매 종료 이벤트 수신: " + event);
         // 포인트 반환, 낙찰자 확정 등 처리 로직 추가 예정
+        // 1. 경매 상태 변경 (closed)
+        auctionCommandService.closeAuction(event.getAuctionId());
+    }*/
+    public void handleAuctionEnd(AuctionEvent event) {
+        try {
+            System.out.println("경매 종료 이벤트 수신: " + event);
+            auctionCommandService.closeAuction(event.getAuctionId());
+        } catch (Exception e) {
+            System.out.println("메시지 처리 실패 - auctionId: " + event.getAuctionId());
+            e.printStackTrace(); // 메시지를 재큐잉 하지 않도록 예외 소비
+        }
     }
 }

03455bcf7bc976310b26836f8c38dd4185356d96
seokhoonhyeon <seokhoon.hyeon@gmail.com>
2025-11-13 15:49:22 +0900

    rebase

--- src/main/java/org/example/lastcall/domain/bid/exception/BidErrorCode.java
+++ src/main/java/org/example/lastcall/domain/bid/exception/BidErrorCode.java
@@ -8,8 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum BidErrorCode implements ErrorCode {
-    SELLER_CANNOT_BID(HttpStatus.FORBIDDEN, "판매자는 자신의 경매에 입찰할 수 없습니다."),
-    BID_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 비드가 존재하지 않습니다.");
+	SELLER_CANNOT_BID(HttpStatus.FORBIDDEN, "판매자는 자신의 경매에 입찰할 수 없습니다."),    // 403 (접근 금지)
+	BID_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 비드가 존재하지 않습니다."),
+    BID_AMOUNT_TOO_LOW(HttpStatus.BAD_REQUEST, "입찰 금액이 현재 최고가 이하입니다.");
 
     private final HttpStatus status;
     private final String message;

161f85a668372f93f12cbd0b9afcd70be1ba9b24
jinhee kim <lsa68803@naver.com>
2025-11-18 16:53:16 +0900

    refactor/auction: 사용되지 않는 에러코드 삭제 (remove unused error code)

--- src/main/java/org/example/lastcall/domain/auction/exception/AuctionErrorCode.java
+++ src/main/java/org/example/lastcall/domain/auction/exception/AuctionErrorCode.java
@@ -15,10 +15,8 @@ public enum AuctionErrorCode implements ErrorCode {
     AUCTION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 경매가 존재하지 않습니다."),
     CANNOT_BID_ON_NON_ONGOING_AUCTION(HttpStatus.FORBIDDEN, "진행 중인 경매일 경우에만 입찰이 가능합니다."),
     CANNOT_MODIFY_ONGOING_OR_CLOSED_AUCTION(HttpStatus.FORBIDDEN, "시작되지 않은 경매만 수정하거나 삭제할 수 있습니다."),
-    USER_NOT_PARTICIPATED_IN_AUCTION(HttpStatus.NOT_FOUND, "해당 경매에 참여한 사용자가 아닙니다."),
     AUCTION_ALREADY_CLOSED(HttpStatus.BAD_REQUEST, "이미 종료된 경매입니다."),
 
-    INVALID_START_TIME(HttpStatus.BAD_REQUEST, "시작일은 현재 시각 이후여야 합니다."),
     INVALID_END_TIME_ORDER(HttpStatus.BAD_REQUEST, "종료일은 시작일 이후여야 합니다."),
     INVALID_SAME_TIME(HttpStatus.BAD_REQUEST, "시작일과 종료일이 같을 수 없습니다.");
 
